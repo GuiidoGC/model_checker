91b8c28ca2ab59236b0cf5f9b87d11d06de2d561
Guido Gonzalez <guidogoca@gmail.com>
2025-01-08 18:59:11 +0100

    Improve error handling and progress feedback in drag_me.py

--- drag_me.py
+++ drag_me.py
@@ -7,30 +7,46 @@ import shutil
 import maya.cmds as cmds
 import maya.OpenMaya as om
 from functools import partial
-
-print("hola")
+from random import uniform
+from importlib import reload
 
 def onMayaDroppedPythonFile(*args):
     """
     Runs when file is dropped into the Maya's viewport
     The name of this function is what Maya expects/uses as entry point, so it cannot be changed
     """
-    path = findScriptPath()
-    scripts_path, scripts_folder = findMayaScriptPath()
-    result = moveScriptToPath(path, scripts_path)
-    update_user_setup(scripts_folder)
+    error_find, path = findScriptPath()
+    if error_find == "Error":
+        print("Error Find Script Path")
+        progress_bar("Error")
+        return
+    error_maya, scripts_path, scripts_folder = findMayaScriptPath()
+    if error_maya == "Error":
+        print("Error Find Maya Script Path")
+        progress_bar("Error")
+        return
+    error_move, result = moveScriptToPath(path, scripts_path)
+    if error_move == "Error":
+        print("Error Move Script To Path")
+        progress_bar("Error")
+        return
+    error_user = update_user_setup(scripts_folder)
+    if error_user == "Error":
+        print("Error User Setup")
+        progress_bar("Error")
+        return
     add_script_to_shelf()
-
-
-    # Initial Feedback
-    print("_"*40)
-    print("Initializing Drag-and-Drop Setup...")
-
+    print(error_find, error_maya, error_move, error_user)
+    if error_find == "Error" or error_maya == "Error" or error_move == "Error" or error_user == "Error":
+        progress_bar("Error")
+    else:
+        progress_bar(None)
 
 def update_user_setup(scripts_path):
     user_setup_name = "userSetup.py"
     
     code_to_add = """
+import maya.cmds as cmds
 print("Dentro")
 if cmds.menu("ModelChecker", exists=True):
     cmds.deleteUI("ModelChecker")
@@ -47,18 +63,19 @@ cmds.menuItem(label="Model Checker", command=partial(import model_checker))
             setup.append(file)
     
     if setup:
-        print(len(setup))
         if len(setup) == 1:
             user_setup_path = os.path.join(scripts_path,user_setup_name)
-            print(user_setup_path)
             with open(user_setup_path, 'a') as file:
                 file.write(code_to_add)
             om.MGlobal.displayInfo("Code added to userSetup.py")
         else:
-            print(setup)
             om.MGlobal.displayError("More than one userSetup.py, skipping!")
             return
-            
+    else:
+        user_setup_path = os.path.join(scripts_path,user_setup_name)
+        with open(user_setup_path, 'w') as file:
+            file.write(code_to_add)
+        om.MGlobal.displayInfo("userSetup.py created and code added")      
 
 
 
@@ -69,8 +86,11 @@ def findScriptPath(*args):
     """
 
     parent_dir = os.path.abspath(__file__).replace('\drag_me.py', '')
+    if parent_dir:
+        return None, parent_dir
+    else:
+        return "Error Find Script Path", parent_dir
     
-    return parent_dir
 
 def findMayaScriptPath(*args):
     """
@@ -86,8 +106,14 @@ def findMayaScriptPath(*args):
 
     scripts_path = final_path
     final_path = os.path.join(final_path, 'model_checker')
+    if os.path.exists(final_path):
+        return "Error Find Maya Script Path", final_path, scripts_path
 
-    return final_path, scripts_path
+
+    if scripts_path and final_path:
+        return None, final_path, scripts_path
+    else:
+        return "Error Find Maya Script Path", final_path, scripts_path
 
 
 def moveScriptToPath(source_path, target_path, *args):
@@ -97,12 +123,17 @@ def moveScriptToPath(source_path, target_path, *args):
     try:
         # Copy the entire directory tree
         shutil.copytree(source_path, target_path)
-        return "Successfully installed"
+        return None, "Success"
     except Exception as e:
-        return f"Failed to move: {e}"
+        return "Error move Script To Path", "Error"
 
 def call_window(*args):
+    try:
         import model_checker
+        reload(model_checker)
+    except ImportError as e:
+        om.MGlobal.displayError("Module not found")
+        return
 
 
 def add_script_to_shelf():
@@ -118,4 +149,38 @@ def add_script_to_shelf():
 
     # Add menu items
     cmds.menuItem(label="Model Checker",command=call_window)
-onMayaDroppedPythonFile()
+
+def progress_bar(error_type):
+    amount = 0
+
+    cmds.progressWindow(    title='Model Checker installer',
+                                            progress=amount,
+                                            status='Installing!',
+                                            isInterruptable=True )
+
+    if error_type == "Error":
+        max_number = 60
+    else:
+        max_number = 100
+
+    while amount < max_number :
+            # Check if the dialog has been cancelled
+            if cmds.progressWindow( query=True, isCancelled=True ) :
+                    break
+
+            # Check if end condition has been reached
+            if cmds.progressWindow( query=True, progress=True ) >= 100 :
+                    break
+
+            amount += uniform(0.1, 5)
+
+
+            cmds.progressWindow( edit=True, progress=round(amount, 2), status=(f"Installing ") )
+
+            cmds.pause( seconds=uniform(0.5, 1.5) )
+
+    cmds.progressWindow(endProgress=1)
+    if error_type == "Error":
+        cmds.confirmDialog(title='Installation Failed', message='Installation failed!', button=['OK'])
+    else:
+        cmds.confirmDialog(title='Installation Complete', message='Installation complete!', button=['OK'])
